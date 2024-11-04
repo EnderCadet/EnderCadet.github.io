2adce87b59304027be9b7fb98c7dbecc58479d09
EnderCadet <landon.mahaffey@svu.edu>
2024-11-04 14:53:38 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,7 @@
-//
+var current = 'page1';
+
+function showit(var1) {
+document.getElementById(current).style.display = 'none';
+document.getElementById(var1).style.display = 'block';
+current = var1;
+}
